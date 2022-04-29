648bb430cd667741a89244023bc2b52fe5e26350
zDR9YHt5ooXU <git1@kaiguo.ca>
2022-04-29 13:41:57 -0700

    2 files modified, 2 added

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,11 @@
 import 'zone.js/dist/zone-testing';
-// import 'zone.js/dist/zone';
+import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/proxy';
-// import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/jasmine-patch';
 
 import './app/system-dashboard/system-dashboard.component.spec.ts';
 
